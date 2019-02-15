d16f50e731be3c14bc956ab5056c20f9cb258f72
Fatih Canbekli <fatih.canbekli@outlook.com>
2019-02-15 14:00:03 +0300

    v0.1

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    [SerializeField] float cameraFlowSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraMovement();
+	}
+
+    private void CameraMovement(){
+        var deltaY = 0.08f;
+        var newXPosition = transform.position.x;
+        var newYPosition = transform.position.y + deltaY;
+        var newZPosition = transform.position.z; //This line because of transform position need vec3.
+        transform.position = new Vector3(newXPosition, newYPosition, newZPosition);
+    }
+}
---
+++ Assets/Scripts/FuelStation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelStation : MonoBehaviour {
+    [SerializeField] GameObject Player;
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.name == "Player")
+        {
+            var fuel = collider.gameObject.GetComponent<PlayerScript>().fuelCapacity;
+            fuel = fuel + 1;
+            collider.gameObject.GetComponent<PlayerScript>().fuelCapacity = fuel;
+        }else if(collider.gameObject.name == "PlayerLaser")
+        {
+            int playerScore = collider.gameObject.GetComponent<PlayerScript>().score;
+            playerScore = playerScore + 100;
+            collider.gameObject.GetComponent<PlayerScript>().score = playerScore;
+            Destroy(collider.gameObject);
+            Destroy(gameObject);
+        }
+        //Sürekli fueli arttırıyor. Fueli düzenli bir şekilde arttırmanın yolunu bul
+    }
+
+}
